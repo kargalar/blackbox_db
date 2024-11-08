eb0d4db28deebbe8c8e6b949e3d8372659b4667e
Kargalar <m.islam0422@gmail.com>
2024-11-08 22:34:39 +0300

    content use ractions

--- lib/3 Page/Content/Widget/content_user_action.dart
+++ lib/3 Page/Content/Widget/content_user_action.dart
@@ -13,152 +13,156 @@ class ContentUserAction extends StatelessWidget {
   Widget build(BuildContext context) {
     late final contentPageProvider = context.watch<ContentPageProvider>();
 
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: AppColors.borderRadiusAll,
-        color: AppColors.panelBackground,
-      ),
-      padding: const EdgeInsets.all(15),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          RatingBar.builder(
-            initialRating: contentPageProvider.contentModel.rating,
-            minRating: 0.5,
-            direction: Axis.horizontal,
-            glow: false,
-            allowHalfRating: true,
-            itemCount: 5,
-            itemSize: 35,
-            itemBuilder: (context, _) => Icon(
-              Icons.star,
-              color: AppColors.main,
-            ),
-            onRatingUpdate: (rating) {
-              contentPageProvider.rate(rating);
-            },
-          ),
-
-          // actions
-          Row(
-            children: [
-              // watch
-              InkWell(
-                onTap: () {
-                  contentPageProvider.watch();
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: Icon(
-                    Icons.remove_red_eye,
-                    color: contentPageProvider.contentModel.isWatch ? AppColors.main : null,
-                    size: 30,
-                  ),
-                ),
+    return Padding(
+      padding: const EdgeInsets.all(25),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: AppColors.borderRadiusAll,
+          color: AppColors.panelBackground,
+        ),
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            RatingBar.builder(
+              initialRating: contentPageProvider.contentModel.rating,
+              minRating: 0.5,
+              direction: Axis.horizontal,
+              glow: false,
+              allowHalfRating: true,
+              itemCount: 5,
+              itemSize: 35,
+              itemBuilder: (context, _) => Icon(
+                Icons.star,
+                color: AppColors.main,
               ),
+              onRatingUpdate: (rating) {
+                contentPageProvider.rate(rating);
+              },
+            ),
 
-              // favori
-              InkWell(
-                onTap: () {
-                  contentPageProvider.favorite();
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: Icon(
-                    Icons.favorite,
-                    color: contentPageProvider.contentModel.isFavorite ? AppColors.main : null,
-                    size: 30,
+            // actions
+            Row(
+              children: [
+                // watch
+                InkWell(
+                  onTap: () {
+                    contentPageProvider.watch();
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Icon(
+                      Icons.remove_red_eye,
+                      color: contentPageProvider.contentModel.isWatch ? AppColors.main : null,
+                      size: 30,
+                    ),
                   ),
                 ),
+
+                // favori
+                InkWell(
+                  onTap: () {
+                    contentPageProvider.favorite();
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Icon(
+                      Icons.favorite,
+                      color: contentPageProvider.contentModel.isFavorite ? AppColors.main : null,
+                      size: 30,
+                    ),
+                  ),
+                ),
+                // wathlater
+                InkWell(
+                  onTap: () {
+                    contentPageProvider.watchLater();
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Icon(
+                      Icons.watch_later,
+                      color: contentPageProvider.contentModel.isWatchLater ? AppColors.main : null,
+                      size: 30,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            // add review
+            InkWell(
+              onTap: () {
+                // add review
+              },
+              child: const Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(5),
+                    child: Icon(
+                      Icons.rate_review,
+                      size: 30,
+                    ),
+                  ),
+                  Text(
+                    "Add Review",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
               ),
-              // wathlater
-              InkWell(
-                onTap: () {
-                  contentPageProvider.watchLater();
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: Icon(
-                    Icons.watch_later,
-                    color: contentPageProvider.contentModel.isWatchLater ? AppColors.main : null,
-                    size: 30,
-                  ),
-                ),
-              )
-            ],
-          ),
-          // add review
-          InkWell(
-            onTap: () {
-              // add review
-            },
-            child: const Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(5),
-                  child: Icon(
-                    Icons.rate_review,
-                    size: 30,
-                  ),
-                ),
-                Text(
-                  "Add Review",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-              ],
             ),
-          ),
 
-          // edit or add log
-          InkWell(
-            onTap: () {
-              // add log
-            },
-            child: const Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(5),
-                  child: Icon(
-                    Icons.edit,
-                    size: 30,
+            // edit or add log
+            InkWell(
+              onTap: () {
+                // add log
+              },
+              child: const Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(5),
+                    child: Icon(
+                      Icons.edit,
+                      size: 30,
+                    ),
                   ),
-                ),
-                Text(
-                  "Edit or Add Log",
-                  style: TextStyle(
-                    fontSize: 20,
+                  Text(
+                    "Edit or Add Log",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
 
-          // add to list
-          InkWell(
-            onTap: () {
-              // add to list
-            },
-            child: const Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(5),
-                  child: Icon(
-                    Icons.playlist_add,
-                    size: 30,
+            // add to list
+            InkWell(
+              onTap: () {
+                // add to list
+              },
+              child: const Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(5),
+                    child: Icon(
+                      Icons.playlist_add,
+                      size: 30,
+                    ),
                   ),
-                ),
-                Text(
-                  "Add to List",
-                  style: TextStyle(
-                    fontSize: 20,
+                  Text(
+                    "Add to List",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
