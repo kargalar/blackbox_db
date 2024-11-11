d604fc80c585d177ae558080974a7fbfa9588772
Kargalar <m.islam0422@gmail.com>
2024-11-11 12:00:10 +0300

    server manager

--- lib/3 Page/Appbar/main_appbar.dart
+++ lib/3 Page/Appbar/main_appbar.dart
@@ -2,6 +2,7 @@ import 'package:blackbox_db/3%20Page/Appbar/Widget/appbar_explore_button.dart';
 import 'package:blackbox_db/3%20Page/Appbar/Widget/appbar_logo.dart';
 import 'package:blackbox_db/3%20Page/Appbar/Widget/appbar_profile.dart';
 import 'package:blackbox_db/3%20Page/Appbar/Widget/appbar_search.dart';
+import 'package:blackbox_db/5%20Service/server_manager.dart';
 import 'package:flutter/material.dart';
 
 class MainAppbar extends StatelessWidget implements PreferredSizeWidget {
@@ -17,11 +18,52 @@ class MainAppbar extends StatelessWidget implements PreferredSizeWidget {
       leading: const AppbarLogo(),
       title: const AppbarSearch(),
       actions: const [
+        // test button
+        TestButton(),
         AppbarExploreButtons(),
         SizedBox(width: 10),
         AppbarProfile(),
         SizedBox(width: 10),
       ],
     );
+  }
+}
+
+// test button get data
+class TestButton extends StatefulWidget {
+  const TestButton({super.key});
+
+  @override
+  State<TestButton> createState() => _TestButtonState();
+}
+
+class _TestButtonState extends State<TestButton> {
+  bool? isLoading;
+
+  ServerManager serverManager = ServerManager();
+
+  late List genres;
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading == null
+        ? ElevatedButton(
+            onPressed: () async {
+              try {
+                // get genres
+                var asdasd = await serverManager.getGenres();
+                setState(() {
+                  genres = asdasd;
+                  isLoading = false;
+                });
+              } catch (e) {
+                debugPrint(e.toString());
+              }
+            },
+            child: const Text('Get Genres'),
+          )
+        : isLoading!
+            ? const CircularProgressIndicator()
+            : Text('Genres: $genres');
   }
 }
--- lib/5 Service/server_manager.dart
+++ lib/5 Service/server_manager.dart
@@ -1,13 +1,27 @@
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ServerManager {
-  static const String _baseUrl = 'https://api.example.com';
+  static const String _baseUrl = 'http://localhost:3000';
 
   // void addUser() {
-
   // }
 
   void getUser() {
     http.get(Uri.parse('$_baseUrl/user'));
+  }
+
+  Future<dynamic> getGenres() async {
+    final response = await http.get(Uri.parse("$_baseUrl/genre"));
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      debugPrint(data);
+      return data;
+    } else {
+      debugPrint("Error: ${response.statusCode}");
+      throw response.statusCode;
+    }
   }
 }
