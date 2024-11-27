e5b3e9beafc8e1c755e345e74601983b2f7d215e
Kargalar <m.islam0422@gmail.com>
2024-11-27 23:31:08 +0300

    CachedNetworkImage

--- lib/2 General/Widget/Content/Widget/content_poster.dart
+++ lib/2 General/Widget/Content/Widget/content_poster.dart
@@ -1,15 +1,19 @@
 import 'package:blackbox_db/2%20General/app_colors.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ContentPoster extends StatelessWidget {
   const ContentPoster({
     super.key,
     required this.posterPath,
     this.size = 150,
+    this.cacheSize = 300,
   });
 
   final String? posterPath;
   final double size;
+  final int cacheSize;
 
   const ContentPoster.showcase({
     Key? key,
@@ -18,6 +22,7 @@ class ContentPoster extends StatelessWidget {
           key: key,
           posterPath: posterPath,
           size: 150,
+          cacheSize: 700,
         );
 
   const ContentPoster.contentPage({
@@ -27,6 +32,7 @@ class ContentPoster extends StatelessWidget {
           key: key,
           posterPath: posterPath,
           size: 230,
+          cacheSize: 2000,
         );
 
   @override
@@ -41,9 +47,33 @@ class ContentPoster extends StatelessWidget {
       child: ClipRRect(
         borderRadius: AppColors.borderRadiusAll / 2,
         child: posterPath != null
-            ? Image.network(
-                "https://image.tmdb.org/t/p/original$posterPath",
+            ? CachedNetworkImage(
+                imageUrl: "https://image.tmdb.org/t/p/original$posterPath",
                 fit: BoxFit.cover,
+                fadeInDuration: const Duration(milliseconds: 50),
+                memCacheHeight: cacheSize,
+                placeholder: (context, url) {
+                  // shimmer
+                  return Shimmer.fromColors(
+                    baseColor: AppColors.deepBlack,
+                    highlightColor: AppColors.panelBackground,
+                    period: const Duration(seconds: 2),
+                    child: Container(
+                      color: AppColors.transparantBlack,
+                    ),
+                  );
+                },
+                errorWidget: (context, url, error) => Container(
+                  color: AppColors.panelBackground,
+                  child: const Center(
+                    child: Text(
+                      "Something",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
               )
             :
             // not found
