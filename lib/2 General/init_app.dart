e9dddefcbc703a9d4a75ce849e76ff1cd36c8e7e
Kargalar <m.islam0422@gmail.com>
2024-12-15 13:44:11 +0300

    isWindows

--- lib/2 General/init_app.dart
+++ lib/2 General/init_app.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -22,7 +23,8 @@ Future<void> initApp(List<String> args) async {
   // isFirstLogin = prefs.getBool('isFirstLogin') ?? true;
 
   // block multiple instances app
-  if (!kIsWeb) {
+  // is windows
+  if (Platform.isWindows) {
     await WindowsSingleInstance.ensureSingleInstance(args, "multiple_timer", onSecondWindow: (args) {
       debugPrint(args.toString());
     });
