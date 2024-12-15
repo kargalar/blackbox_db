783d07be0d5e86e4d89d6b2d9e99f29f0d00babe
Kargalar <m.islam0422@gmail.com>
2024-12-15 13:46:44 +0300

    fix2

--- lib/2 General/init_app.dart
+++ lib/2 General/init_app.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -24,10 +25,11 @@ Future<void> initApp(List<String> args) async {
   // block multiple instances app
   // is windows
   if (!kIsWeb) {
-    await WindowsSingleInstance.ensureSingleInstance(args, "multiple_timer", onSecondWindow: (args) {
-      debugPrint(args.toString());
-    });
-
+    if (Platform.isWindows) {
+      await WindowsSingleInstance.ensureSingleInstance(args, "multiple_timer", onSecondWindow: (args) {
+        debugPrint(args.toString());
+      });
+    }
     // Must add this line.
     await windowManager.ensureInitialized();
 
