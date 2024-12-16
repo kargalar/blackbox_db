67ef8e2152fdfcc3bfae7c9022d8b2fdab890447
Kargalar <m.islam0422@gmail.com>
2024-12-16 12:44:00 +0300

    home page page index fix

--- lib/3 Page/Home/home_page.dart
+++ lib/3 Page/Home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:blackbox_db/2%20General/accessible.dart';
 import 'package:blackbox_db/3%20Page/Explore/Widget/content_list.dart';
 import 'package:blackbox_db/5%20Service/server_manager.dart';
+import 'package:blackbox_db/6%20Provider/explore_provider.dart';
 import 'package:blackbox_db/7%20Enum/content_type_enum.dart';
 import 'package:blackbox_db/7%20Enum/showcase_type_enum.dart';
 import 'package:blackbox_db/8%20Model/showcase_movie_model.dart';
@@ -76,6 +77,8 @@ class _HomePageState extends State<HomePage> {
 
   void getContent() async {
     try {
+      ExploreProvider().currentPageIndex = 0;
+
       // TODO: widget.showcaseType a göre farklı endpointlere istek atacak
       // TODO: mesela trend ise sadece 5 tane getirecek. actviity ise contentlogmodel için de veri getirecek...
       // TODO: contentType null ise farklı istek atacak
