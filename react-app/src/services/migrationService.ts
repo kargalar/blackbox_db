e0d742ec303191a41553a5316936d031644d0807
kargalar <m.islam0422@gmail.com>
2025-09-04 08:37:11 +0300

    delete react2

--- react-app/src/services/migrationService.ts
+++
@@ -1,5 +0,0 @@
-// Supabase migration service stub - to be implemented
-export async function getAverageRatingsForContentIds(ids: string[]): Promise<Record<string, number>> {
-  // TODO: Fetch from Supabase RPC/view
-  return Object.fromEntries(ids.map((id) => [id, Math.round(Math.random() * 80) / 10]));
-}
